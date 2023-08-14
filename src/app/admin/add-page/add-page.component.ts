7cd06b6a570c61368ba78ea1cbf6d7a927899bcc
alphastation <alexander111111@gmail.com>
2023-08-14 20:45:35 +0100

    Page for adding new products implemented within add-page component,respectively submit function in the ts file and  form fields in the html file;for some particular fields like photh new module named quill imported, together with the npm ngx-quill packet, scss file imported as well.

--- src/app/admin/add-page/add-page.component.ts
+++ src/app/admin/add-page/add-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-page',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-page.component.scss']
 })
 export class AddPageComponent {
+  form: FormGroup;
+submitted = false;
+  constructor() { }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      type: new FormControl(null, Validators.required),
+      title: new FormControl(null, Validators.required),
+      photo: new FormControl(null, Validators.required),
+      info: new FormControl(null, Validators.required),
+      price: new FormControl(null, Validators.required)
+
+    })
+
+
+
+  }
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+    const product = {
+      type: this.form.value.type,
+      title: this.form.value.title,
+      photo: this.form.value.photo,
+      info: this.form.value.info,
+      price: this.form.value.price,
+
+
+    }
+
+    console.log(this.form)
+    console.log(product)
+
+
+  }
 
 }
